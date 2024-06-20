e2e2661eaec6f01c00950e69c1d5441fe42bdabf
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-20 19:33:39 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let s = 0; s < names.length; s++){
+        thankYouMessages.push(`Thank you, ${names[s]}, for the wonderful ${event} gift!`);
+    }
+
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
+countDown(4);
